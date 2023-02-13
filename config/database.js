3d9e5b14d38a75eff21ddeeb125c8472161ab4cf
dandesign21 <50307591+dandesign21@users.noreply.github.com>
2023-02-13 17:28:13 -0600

    DB  env conf

--- config/database.js
+++ config/database.js
@@ -1,10 +1,14 @@
-const path = require('path');
+// path: ./my-project/config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "strapi.co1pmnfpjblg.us-east-2.rds.amazonaws.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", ".1Programacion1."),
     },
     useNullAsDefault: true,
   },
